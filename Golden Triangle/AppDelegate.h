ef65f53c511100f345e8b9fa8a701a0569e06d92
= <luke.s.campbell@gmail.com>
2013-11-28 11:55:18 -0500

    Initial Commit

---
+++ Golden Triangle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Golden Triangle
+//
+//  Created by Lucas Campbell on 11/28/13.
+//  Copyright (c) 2013 Lucas Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Golden Triangle/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Golden Triangle
+//
+//  Created by Lucas Campbell on 11/28/13.
+//  Copyright (c) 2013 Lucas Campbell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
